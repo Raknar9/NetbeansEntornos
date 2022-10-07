2b1f21bfb65472a49c528fcd375da4d0ff278cde
Raknar9 <aguilejan99@gmail.com>
2022-10-08 00:51:11 +0200

    netbeans proyects

--- src/main/java/com/mycompany/ohog/Ohog.java
+++ src/main/java/com/mycompany/ohog/Ohog.java
@@ -12,6 +12,6 @@ package com.mycompany.ohog;
 public class Ohog {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! ");
     }
 }
